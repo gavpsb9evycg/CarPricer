9b9263f5cd899b45df0a11801784d2e79c451410
gavpsb9evycg <69355478+gavpsb9evycg@users.noreply.github.com>
2020-11-03 23:49:36 +0300

    Add project files.

---
+++ CarPricer/PriceDeterminator.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace CarPricer
+{
+    public class PriceDeterminator
+    {
+        public decimal DetermineCarPrice(Car car)
+        {
+            decimal adjustedPrice = AdjustPriceForAge(car.AgeInMonths, car.PurchaseValue);
+            adjustedPrice = AdjustPriceForMiles(car.NumberOfMiles, adjustedPrice);
+            adjustedPrice = AdjustPriceForCollision(car.NumberOfCollisions, adjustedPrice);
+            adjustedPrice = AdjustPriceForPreviousOwnerReducing(car.NumberOfPreviousOwners, adjustedPrice);
+            adjustedPrice = AdjustPriceForReliability(car.Make, adjustedPrice);
+            adjustedPrice = AdjustPriceForPreviousOwnerAdding(car.NumberOfPreviousOwners, adjustedPrice);
+            adjustedPrice = AdjustPriceForProfitability(car.PurchaseValue, adjustedPrice);
+
+            return adjustedPrice;
+        }
+
+        private decimal AdjustPriceForAge(int ageInMonths, decimal carPrice)
+        {
+            int tenYearsInMonths = 10 * 12;
+            decimal adjustedPrice = carPrice - carPrice / 100 * 0.5m * Math.Min(ageInMonths, tenYearsInMonths);
+
+            return adjustedPrice;
+        }
+
+        private decimal AdjustPriceForMiles(int numberOfMiles, decimal carPrice)
+        {
+            int limitOfMiles = 150000;
+            decimal adjustedPrice = carPrice - carPrice / 100 * 0.2m * Math.Min(numberOfMiles, limitOfMiles) / 1000;
+
+            return adjustedPrice;
+        }
+
+        private decimal AdjustPriceForCollision(int numberOfCollisions, decimal carPrice)
+        {
+            int limitOfCollisions = 5;
+            decimal adjustedPrice = carPrice - carPrice / 100 * 2 * Math.Min(numberOfCollisions, limitOfCollisions);
+
+            return adjustedPrice;
+        }
+
+        private decimal AdjustPriceForPreviousOwnerReducing(int numberOfPreviousOwners, decimal carPrice)
+        {
+            decimal adjustedPrice = carPrice;
+
+            int criticalNumberOfPreviousOwners = 2;
+            int criticalReducingPercent = 25;
+
+            if (numberOfPreviousOwners > criticalNumberOfPreviousOwners)
+            {
+                adjustedPrice = carPrice - carPrice / 100 * criticalReducingPercent;
+            }
+
+            return adjustedPrice;
+        }
+
+        private decimal AdjustPriceForPreviousOwnerAdding(int numberOfPreviousOwners, decimal carPrice)
+        {
+            decimal adjustedPrice = carPrice;
+
+            int noPreviousOwners = 0;
+            int addingPercent = 10;
+
+            if (numberOfPreviousOwners == noPreviousOwners)
+            {
+                adjustedPrice = carPrice + carPrice / 100 * addingPercent;
+            }
+
+            return adjustedPrice;
+        }
+
+        private decimal AdjustPriceForReliability(string make, decimal carPrice)
+        {
+            if (string.IsNullOrWhiteSpace(make))
+                return carPrice;
+
+            decimal adjustedPrice = carPrice;
+
+            string makeToyota = "Toyota";
+            int ToyotaReliabilityPercent = 5;
+
+            if (make.Equals(makeToyota, StringComparison.OrdinalIgnoreCase))
+            {
+                adjustedPrice = carPrice + carPrice / 100 * ToyotaReliabilityPercent;
+            }
+
+            string makeFord = "Ford";
+            int FordReliabilitySubtractSum = 500;
+
+            if (make.Equals(makeFord, StringComparison.OrdinalIgnoreCase))
+            {
+                adjustedPrice = carPrice - FordReliabilitySubtractSum;
+            }
+
+            return adjustedPrice;
+        }
+
+        private decimal AdjustPriceForProfitability(decimal purchaseValue, decimal carPrice)
+        {
+            decimal adjustedPrice = carPrice;
+
+            int profitabilityPercent = 90;
+            decimal profitabilityPrice = purchaseValue / 100 * profitabilityPercent;
+
+            if (carPrice > profitabilityPrice)
+            {
+                adjustedPrice = profitabilityPrice;
+            }
+
+            return adjustedPrice;
+        }
+    }
+}
---
+++ CarPricer/ReadMe.txt
@@ -0,0 +1,102 @@
+﻿https://storage.pardot.com/101722/99984/CarPricer.txt
+
+#region Instructions
+/*
+ * You are tasked with writing an algorithm that determines the value of a used car, 
+ * given several factors.
+ *
+ *    AGE:    Given the number of months of how old the car is, reduce its value one-half 
+ *            (0.5) percent.
+ *            After 10 years, it's value cannot be reduced further by age. This is not 
+ *            cumulative.
+ *            
+ *    MILES:    For every 1,000 miles on the car, reduce its value by one-fifth of a
+ *              percent (0.2). Do not consider remaining miles. After 150,000 miles, it's 
+ *              value cannot be reduced further by miles.
+ *            
+ *    PREVIOUS OWNER:    If the car has had more than 2 previous owners, reduce its value 
+ *                       by twenty-five (25) percent. If the car has had no previous  
+ *                       owners, add ten (10) percent of the FINAL car value at the end.
+ *                    
+ *    COLLISION:        For every reported collision the car has been in, remove two (2) 
+ *                      percent of it's value up to five (5) collisions.
+ *
+ *    RELIABILITY:      If the Make is Toyota, add 5%.  If the Make is Ford, subtract $500.
+ *
+ *
+ *    PROFITABILITY:    The final calculated price cannot exceed 90% of the purchase price. 
+ *    
+ * 
+ *    Each factor should be off of the result of the previous value in the order of
+ *        1. AGE
+ *        2. MILES
+ *        3. PREVIOUS OWNER
+ *        4. COLLISION
+ *        5. RELIABILITY
+ *        
+ *    E.g., Start with the current value of the car, then adjust for age, take that  
+ *    result then adjust for miles, then collision, previous owner, and finally reliability. 
+ *    Note that if previous owner, had a positive effect, then it should be applied 
+ *    AFTER step 5. If a negative effect, then BEFORE step 5.
+ */
+#endregion
+
+using System;
+using NUnit.Framework;
+
+namespace CarPricer
+{
+    public class Car
+    {
+        public decimal PurchaseValue { get; set; }
+        public int AgeInMonths { get; set; }
+        public int NumberOfMiles { get; set; }
+        public int NumberOfPreviousOwners { get; set; }
+        public int NumberOfCollisions { get; set; }
+        public string Make { get; set; }
+    }
+
+    public class PriceDeterminator
+    {
+        public decimal DetermineCarPrice(Car car)
+        {
+            //TODO: Implement this method
+            return 0;
+        }
+    }
+
+    
+    [TestFixture]
+    public class UnitTests
+    {
+        
+        [Test]
+        public void CalculateCarValue()
+        {
+            AssertCarValue(24813.40m, 35000m, 3 * 12, 50000,  1, 1, "Ford");
+            AssertCarValue(20672.61m, 35000m, 3 * 12, 150000, 1, 1, "Toyota");
+            AssertCarValue(19688.20m, 35000m, 3 * 12, 250000, 1, 1, "Tesla");
+            AssertCarValue(21094.5m, 35000m, 3 * 12, 250000, 1, 0, "toyota");
+            AssertCarValue(21657.02m, 35000m, 3 * 12, 250000, 0, 1, "Acura");
+            AssertCarValue(72000m, 80000m, 8, 10000, 0, 1, null);
+        }
+
+        private static void AssertCarValue(decimal expectValue, decimal purchaseValue, 
+        int ageInMonths, int numberOfMiles, int numberOfPreviousOwners, int 
+        numberOfCollisions, string make)
+        {
+            Car car = new Car
+                        {
+                            AgeInMonths = ageInMonths,
+                            NumberOfCollisions = numberOfCollisions,
+                            NumberOfMiles = numberOfMiles,
+                            NumberOfPreviousOwners = numberOfPreviousOwners,
+                            PurchaseValue = purchaseValue,
+                            Make = make
+                        };
+            PriceDeterminator priceDeterminator = new PriceDeterminator();
+            var carPrice = priceDeterminator.DetermineCarPrice(car);
+            Assert.AreEqual(expectValue, carPrice);
+        }
+    }
+}
